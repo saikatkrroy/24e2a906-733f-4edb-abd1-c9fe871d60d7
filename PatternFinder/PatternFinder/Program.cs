03ae2b0a58ace85a969b6b6ba29b934ec432cd55
Saikat Roy <saikat.roy@servicestream.com.au>
2026-04-29 12:36:56 +1000

    Added Unit tests and empty project

---
+++ PatternFinder/PatternFinder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternFinder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var lisFinder = new LisFinder();
+            Console.WriteLine("Enter a sequence of integers separated by spaces:");
+            string input = Console.ReadLine() ?? string.Empty;
+            string result = lisFinder.Find(input);
+            Console.WriteLine("The earliest longest increasing contiguous subarray is:");
+            Console.WriteLine(result);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
